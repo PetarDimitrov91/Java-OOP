9e66de233b8aac4a36d47d93b129e5f77294710e
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-07-19 20:36:08 +0200

    dsadsadsad

--- 2. Exercises/9.1 Polymorphism Exercise/src/Test.java
+++ 2. Exercises/9.1 Polymorphism Exercise/src/Test.java
@@ -3,7 +3,7 @@ import java.text.DecimalFormat;
 public class Test {
     public static void main(String[] args) {
 
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        DecimalFormat decimalFormat = new DecimalFormat("#.#####");
 
         double d = 15.001;
 
