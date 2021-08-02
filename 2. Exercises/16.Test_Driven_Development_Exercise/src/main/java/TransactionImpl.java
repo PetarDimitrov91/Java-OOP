da0c1cf876c844d5ef40e3408a8547c2bac1b474
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-08-02 22:50:36 +0200

    dasdad

---
+++ 2. Exercises/16.Test_Driven_Development_Exercise/src/main/java/TransactionImpl.java
@@ -0,0 +1,20 @@
+public class TransactionImpl implements Comparable<TransactionImpl>{
+
+    private int id;
+    private TransactionStatus status;
+    private String from;
+    private String to;
+    private double amount;
+
+    public TransactionImpl(int id, TransactionStatus status, String from, String to, double amount) {
+        this.id = id;
+        this.status = status;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+    public int compareTo(TransactionImpl o) {
+        return 0;
+    }
+}
